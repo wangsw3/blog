e4138e529e7e6b9e7ab05e9bdd224376afdbc108
wangsw3 <wangsw3@asiainfo.com>
2019-12-11 15:43:59 +0800

    swaggerConfig

--- src/main/java/com/wangsw/blog/config/SwaggerConfig.java
+++ src/main/java/com/wangsw/blog/config/SwaggerConfig.java
@@ -1,12 +1,12 @@
 package com.wangsw.blog.config;
 
+
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,33 +21,18 @@ public class SwaggerConfig {
 
     private final String version = "1.0";
 
-    private final String title = "SpringBoot示例工程";
+    private final String title = "blog";
 
-    private final String description = "API文档自动生成示例";
+    private final String description = "API接口";
 
-    private final String termsOfServiceUrl = "http://www.kingeid.com";
-
-    private final String license = "MIT";
-
-    private final String licenseUrl = "https://mit-license.org/";
-
-    private final Contact contact = new Contact("wangsw", "https://github.com/blog", "562495956@qq.com");
 
     @Bean
     public Docket buildDocket() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(buildApiInf())
-                .select().build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(buildApiInf()).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
     }
 
     private ApiInfo buildApiInf() {
-        return new ApiInfoBuilder().title(title).termsOfServiceUrl(termsOfServiceUrl).description(description)
-                .version(version).license(license).licenseUrl(licenseUrl).contact(contact).build();
-
+        return new ApiInfoBuilder().title(title+version).description(description).version(version).build();
     }
-
-    /*@Bean
-    public Docket swaggerSpringMvcPlugin() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
-    }*/
 
 }
